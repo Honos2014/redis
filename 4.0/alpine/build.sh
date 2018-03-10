dccf9145311da0dc3e9c3cac0478ad74413bd31f
Honos2014 <Honos2014@users.noreply.github.com>
2018-03-10 15:29:29 -0500

    Update build.sh

--- 4.0/alpine/build.sh
+++ 4.0/alpine/build.sh
@@ -3,7 +3,7 @@
 # Docker Alpine/ReDis/STunnel
 
 # If needed create the stunnel ssl key
-if [ ! -f /tmp/foo.txt ]; then
+if [ ! -f stunnel.pem ]; then
     echo "Creating stunnel key"
     openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 3650 -nodes -subj "/C=CA/ST=QC/L=Mtl/O=Cie/OU=Org/CN=my.stunnel.home"
     cat key.pem cert.pem >> stunnel.pem
